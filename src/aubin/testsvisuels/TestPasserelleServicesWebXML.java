7494eded68fddfc80e5b88d9644e887a1cc016a3
Heurtault Aubin <aub.heurtault@gmail.com>
2022-03-24 21:04:09 +0100

    init

---
+++ src/aubin/testsvisuels/TestPasserelleServicesWebXML.java
@@ -0,0 +1,35 @@
+package aubin.testsvisuels;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import aubin.classes.Outils;
+import aubin.classes.PasserelleServicesWebXML;
+import aubin.classes.PointDeTrace;
+import aubin.classes.Trace;
+import aubin.classes.Utilisateur;
+
+public class TestPasserelleServicesWebXML {
+
+	public static void main(String[] args) throws ParseException {
+		
+		String msg;
+	
+		// test visuel de la méthode getTousLesUtilisateurs
+
+
+		// test visuel de la méthode getLesUtilisateursQueJautorise
+
+
+		// test visuel de la méthode getLesUtilisateursQuiMautorisent
+
+		
+		// test visuel de la méthode getLesParcoursDunUtilisateur
+
+		
+		// test visuel de la méthode getUnParcoursEtSesPoints
+
+	
+	} // fin Main
+} // fin class
